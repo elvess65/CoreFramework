3a159485bb535d671ac9b6cfde20f89b62f53085
elvess65 <37631917+elvess65@users.noreply.github.com>
2022-02-08 16:08:26 +0200

    1

--- Scripts/TestScript.cs
+++ Scripts/TestScript.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace ingames.testCore
+namespace Ingames.testCore
 {
-    public class TestScript : MonoBehaviour
+    public static class TestScript
     {
         public static void Call()
         {
-            Debug.Log("Call");
+            Debug.Log("ingames Call");
         }
     }
 }

b05093435d775caea5988ae7c0117845e128f87a
elvess65 <37631917+elvess65@users.noreply.github.com>
2022-02-08 15:53:50 +0200

    1

--- Scripts/TestScript.cs
+++ Scripts/TestScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace test.testCore
+namespace ingames.testCore
 {
     public class TestScript : MonoBehaviour
     {

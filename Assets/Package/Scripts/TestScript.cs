4b12b103542c18d5cc6b6c2775d7575e851aec8a
elvess65 <37631917+elvess65@users.noreply.github.com>
2022-02-08 14:37:03 +0200

    Package settings

---
+++ Assets/Package/Scripts/TestScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace test.testCore
+{
+    public class TestScript : MonoBehaviour
+    {
+        public static void Call()
+        {
+            Debug.Log("Call");
+        }
+    }
+}

a20b7efcb248ad56bc01a6f0a7bedc5b09ab303f
elvess65 <elvess65@gmail.com>
2022-08-01 22:25:11 +0300

    Update version

---
+++ Examples/Scripts/Core/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CoreFramework.Examples
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        public SceneLoader SceneLoader { get; private set; }
+
+        public void Initialize()
+        {
+            StartCoroutine(InitializationDelay());
+        }
+
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            SceneLoader = new SceneLoader();
+        }
+
+        private IEnumerator InitializationDelay()
+        {
+            yield return new WaitForSeconds(1);
+
+            ConnectionResultSuccess();
+        }
+
+        private void ConnectionResultSuccess()
+        {
+            SceneLoader.LoadLevel(SceneLoader.MAIN_SCENE_NAME);
+        }
+    }
+}
